2167a8e0ff66ca09df7af63230403eab08decf3f
Mateus Schwede <mateus@debian.debian>
2020-08-17 11:42:36 -0300

    ativar/inativarAtendente finalizado

---
+++ admin/atAtendente.php
@@ -0,0 +1,7 @@
+<?php
+require_once '../conect.php';
+session_start();
+$r = $db->prepare("UPDATE atendente SET ativo=1 WHERE id=?");
+$r->execute(array(base64_decode($_GET['id'])));
+$_SESSION['msg'] = "<br><div class='alert alert-success alert-dismissible fade show' role='alert'>Atendente ativado!<button type='button' class='close' data-dismiss='alert' aria-label='Close'><span aria-hidden='true'>&times;</span></button></div>";
+header("location: atendentes.php");
